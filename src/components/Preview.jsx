3a2ebb381eba3e348269f47d1247067947aa8633
clecardona <clecardona@gmail.com>
2021-11-16 13:44:37 +0100

    preview page

---
+++ src/components/Preview.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Preview() {
+  return (
+    <main className="page-preview">
+      <h1>Preview</h1>
+    </main>
+  );
+}
--- src/routes/Logged.tsx
+++ src/routes/Logged.tsx
@@ -3,6 +3,7 @@ import Admin from "components/AdminPages/Admin";
 import Login from "components/AuthPages/Login";
 import SignUp from "components/AuthPages/Signup";
 import Home from "components/Home";
+import Preview from "components/Preview";
 
 import { Route } from "react-router-dom";
 import { useAuth } from "state/AuthProvider";
@@ -15,6 +16,7 @@ export default function Logged() {
   return (
     <>
       <Route exact path="/" component={Home} />
+      <Route exact path="/preview" component={Preview} />
       <Route component={Login} path="/login" />
       <Route component={SignUp} path="/signup" />
       {user.role === "admin" && <Route component={Admin} path="/admin" />}
