0bea179b84c283d95c1f4898c364b3b92e4e4a79
clecardona <clecardona@gmail.com>
2021-11-20 17:21:43 +0100

    templates

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,64 +1,88 @@
 import mockup from "assets/backup/mockup.json";
+import { useState } from "react";
 
 export default function Preview() {
+  const [style, setStyle] = useState("basic");
+
   const {
     photo,
+    role,
     contact,
     additional_info,
     certification,
     education,
     work_history,
+    highlights,
   } = mockup;
 
-  console.log(certification);
-
-  const Languages = additional_info.languages.map((item, index) => (
-    <li key={index}>
-      <p>{item}</p>{" "}
-    </li>
-  ));
-
-  const Skills = additional_info.additional_skills.map((item, index) => (
+  const Highlights = highlights.map((item, index) => (
     <li key={index}>
       <p>{item}</p>
     </li>
   ));
 
-  const WorkHistory = work_history.map((item, index) => (
+  const Languages = additional_info.languages.map((item, index) => (
     <li key={index}>
-      <h3 className="heading1">
-        {item.title} | {item.from} - {item.to}
-      </h3>
-      <h4 className="heading2">
-        {item.company},{item.city},{item.country}
-      </h4>
-      <p>
-        Because people absorb new information by relating it to an existing
-        reference point they understand, you should create a metaphor that
-        allows them to associate, compare, and draw relations easily.
-      </p>
+      <h4>{item}</h4>{" "}
     </li>
   ));
 
-  const Education = education.map((item, index) => (
+  const Skills = additional_info.additional_skills.map((item, index) => (
     <li key={index}>
-      <h3 className="heading1">
-        {item.program} | {item.duration} year
+      <h4>{item}</h4>
+    </li>
+  ));
+
+  const WorkHistory = work_history.map((item, index) => {
+    const Responsibilities = item.responsibilities.map((item, index) => (
+      <li key={index}>
+        <p>{item}</p>
+      </li>
+    ));
+    const Accomplishments = item.accomplishments.map((item, index) => (
+      <li key={index}>
+        <p>{item}</p>
+      </li>
+    ));
+
+    return (
+      <li key={index}>
+        <div className="heading1">
+          <h3>{item.title}</h3>
+          <h3>
+            {item.from} - {item.to}
+          </h3>
+        </div>
+        <h4 className="heading2">
+          {item.company}, {item.city}, {item.country}
+        </h4>
+        <ul className="responsibilities">{Responsibilities}</ul>
+        <ul className="accomplishments">{Accomplishments}</ul>
+      </li>
+    );
+  });
+
+  const Education = education.map((item, index) => (
+    <li key={index} className="heading1">
+      <div className="data">
+        <h4>
+          {item.school} - {item.program}
+        </h4>
+      </div>
+      <h4>
+        Degree {item.duration} year
         {Number.parseInt(item.duration) > 1 && "s"}
-      </h3>
-      <h4 className="heading2">{item.school}</h4>
-      <p>
-        Because people absorb new information by relating it to an existing
-        reference point they understand, you should create a metaphor that
-        allows them to associate, compare, and draw relations easily.
-      </p>
+      </h4>
     </li>
   ));
 
   const Certification = certification.map((item, index) => (
-    <li key={index}>
-      <h4 className="heading1">
-        {item.domain} | {item.duration} year
+    <li key={index} className="heading1">
+      <div className="data">
+        <h4>{item.domain} </h4>
+      </div>
+      <h4>
+        Certificate {item.duration} year
         {Number.parseInt(item.duration) > 1 && "s"}
       </h4>
     </li>
@@ -67,17 +91,17 @@ export default function Preview() {
   return (
     <main className="page-preview">
       <h1>Preview</h1>
-      <div className="a4">
+      <div className="selector">
+        <button onClick={() => setStyle("basic")}>Basic</button>
+        <button onClick={() => setStyle("modern")}>Modern</button>
+      </div>
+
+      <div className={`a4 ${style}`}>
         <div className="title">
           <h1 className="name">
             {contact.firstname} {contact.middlename} {contact.lastname}
           </h1>
-          <h2 className="role">Role</h2>
-          <p className="pitch">
-            “Our device is called the Hydrolyzier, and it uses a tri-plane
-            osmotic process that dramatically reduces the content of impurities
-            in drinking water.”
-          </p>
+          <h2 className="role">{role}</h2>
         </div>
         {photo && (
           <div className="photo">
@@ -91,26 +115,30 @@ export default function Preview() {
           </h4>
           <h4 className="email">{contact.email}</h4>
           <h4 className="mobile">{contact.mobile}</h4>
+          <h4 className="linkedin">{contact.linkedin}</h4>
         </div>
+
+        <div className="highlights">
+          <h2>Career Highlights</h2>
+          <ul>{Highlights}</ul>
+        </div>
+
         <div className="languages">
-          <h3>Languages spoken :</h3>
+          <h2>Languages spoken</h2>
           <ul>{Languages}</ul>
         </div>
         <div className="skills">
-          <h3>Skills :</h3>
+          <h2>Skills</h2>
           <ul>{Skills}</ul>
         </div>
         <div className="content">
           <div className="work-history">
-            <h3>Professional Experience :</h3>
-            <ul>{WorkHistory}</ul>
+            <h2>Professional Experience</h2>
+            <ul className="work-history">{WorkHistory}</ul>
           </div>
           <div className="education">
-            <h3>Education :</h3>
+            <h2>Education</h2>
             <ul>{Education}</ul>
-          </div>
-          <div className="certification">
-            <h3>Certifications & Training :</h3>
             <ul>{Certification}</ul>
           </div>
         </div>
